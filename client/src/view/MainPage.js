b86dd7e431f315076a324fea94674af9a1d59fd3
seungb1027 <seungb1027@gmail.com>
2022-05-16 21:07:12 +0900

    MainPage.js 수정

--- client/src/view/MainPage.js
+++ client/src/view/MainPage.js
@@ -33,7 +33,7 @@ const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const theme = createTheme();
 
-export default function Album() {
+export default function MainPage() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
