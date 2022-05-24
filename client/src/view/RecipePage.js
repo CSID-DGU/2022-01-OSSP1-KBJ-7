09ef4679ef06c31638f993faab34fee9f31f7808
seungb1027 <seungb1027@gmail.com>
2022-05-24 23:41:40 +0900

    레시피페이지 관련 수정 및 추가

--- client/src/view/RecipePage.js
+++ client/src/view/RecipePage.js
@@ -1,11 +1,72 @@
 import React from 'react';
+import '../component/RecipeLayout.css'
+import Nav from '../component/Nav'
+import Button from '@mui/material/Button';
 
-function RecipePage () {
+// 서버와 연동
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
+export default function RecipePage () {
+  const [foodList, setFoodList] = useState([]);
+
+    // 페이지 렌더링 후 가장 처음 호출되는 함수
+    // 음식 리스트 얻어오기
+    useEffect(()=>{
+      axios.get('/api/foodList')
+      .then(res => setFoodList(res.data))
+      .then(console.log(foodList)) // 받아온 음식리스트 출력해보기
+    }, [])
+
+    //const f1 = foodList.get();
   return (
-    <div>
-      <h1>레시피 설명 페이지</h1>
-    </div>
-  );
-};
+    <>
+      <Nav/>
+      <br></br>
+      <h2 align="center">레시피</h2>
 
-export default RecipePage;
+      <div className="post-view-wrapper">
+        {
+          foodList[0] ? (
+            <>
+              <div className="post-view-row">
+                <label>음식 번호</label>
+                <label>{foodList[0].id}번</label>
+              </div>
+              <div className="post-view-row">
+                <label>음식 이름</label>
+                <label>{foodList[0].name}</label>
+              </div>
+              <div className="post-view-row">
+                <label>칼로리</label>
+                <label>{foodList[0].kcal}kcal</label>
+              </div>
+              <div className="post-view-row">
+                <label>탄수화물</label>
+                <label>{foodList[0].carbohydrate}</label>
+              </div>
+              <div className="post-view-row">
+                <label>단백질</label>
+                <label>{foodList[0].protein}</label>
+              </div>
+              <div className="post-view-row">
+                <label>지방</label>
+                <label>{foodList[0].fat}</label>
+              </div>
+              <div className="post-view-row">
+                <label>레시피</label>
+                <div>
+                  {
+                    foodList[0].make
+                  }
+                </div>
+              </div>
+            </>
+          ) : '해당 게시글을 찾을 수 없습니다.'
+        }
+        <Button variant="contained">메인으로</Button><br></br><br></br>
+        <Button variant="outlined">장바구니</Button>
+      </div>
+    </>
+  );
+}
