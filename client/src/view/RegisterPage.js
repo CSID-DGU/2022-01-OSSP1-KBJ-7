2d91a557fac9a44f9487421c8dd417a6f6a66186
kunhei <84655228+kunhei@users.noreply.github.com>
2022-05-25 12:09:57 +0900

    아이디 입력칸 수 조정

--- client/src/view/RegisterPage.js
+++ client/src/view/RegisterPage.js
@@ -58,7 +58,7 @@ export default function SignUp() {
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
