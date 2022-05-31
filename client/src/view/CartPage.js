0fc4fb895cb5a1615e771b0323a65032e457f5f0
kunhei <84655228+kunhei@users.noreply.github.com>
2022-05-31 17:11:03 +0900

    장바구니 아이템 출력 프론트 구성

--- client/src/view/CartPage.js
+++ client/src/view/CartPage.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+//import Link from '@mui/material/Link';
+import {Link} from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import styled from 'styled-components';
+
 
 import axios from 'axios';
 import { useState, useEffect, useLayoutEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+const theme = createTheme();
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 8px 20px;
+  border-radius: 5px;
+  margin: 5px 0px;
+  cursor: pointer;
+`;
 
 function CartPage() {
     const [Cart, setCart] = useState([]);
@@ -33,7 +62,7 @@ function CartPage() {
             .then(console.log(foodList))
 
     });
-
+    const FoodId = foodList.map((food, id) => food.id); //카드 배열 위해 id 추가
     const FoodImage = foodList.map((food, id) => food.image);
     const FoodName = foodList.map((food, id) => food.name);
     const FoodIngredient = foodList.map((food, id) => food.ingredient);
@@ -43,8 +72,22 @@ function CartPage() {
     const FoodFat = foodList.map((food, id) => food.fat);
 
     return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Healthy Pleasure
+          </Typography>
+        </Toolbar>
+        <Toolbar>
+          <Link to='/Signin'><button>로그인</button></Link>
+          <Link to='/Register'><button>회원가입</button></Link>       
+          <Link to='/Cart/kaka5'><button>장바구니</button></Link> {/* kaka5 유저에 대해 테스트 */}
+        </Toolbar>
+      </AppBar>
         <div>
-            <h1>장바구니 페이지12</h1>
             <br></br>
             {/* {Cart.map((food, index) => (
         // 곤약 떡볶이를 담았는지를 출력해보기
@@ -54,27 +97,50 @@ function CartPage() {
         </div>
       ))} */}
 
-
-            <br></br>
-            <h2>DB의 user_item table을 조회한 모습(순서대로 출력, kaka5는 유저 이름), 출력시간이 좀 걸릴 수 있음</h2>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
             {CartList.map((array, index) => (
-                // 배열로 변환한 음식을 장바구니에 담았는지를 출력
-                <div>
-                    <h2>
-                        {array === 1 ? FoodImage[index - 1] + ' ' + '이름 : ' + FoodName[index - 1] + ' '
-                            + '재료 : ' + FoodIngredient[index - 1] + ' '
-                            + '칼로리 : ' + FoodKcal[index - 1] + ' '
-                            + '영양정보 : 탄수화물 ' + FoodCar[index - 1] + 'g '
-                            + '단백질 ' + FoodPro[index - 1] + 'g '
-                            + '지방 ' + FoodFat[index - 1] + 'g'
-                            : null
-                        }
-                    </h2>
-                </div>
-            ))
-
-            }
+             array==1?
+              <Grid item key={FoodId[index - 1]} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image={FoodImage[index - 1]}
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {/* 음식 이름 */}
+                      {FoodName[index - 1]}
+                    </Typography>
+                    <Typography>
+                      {/* 레시피 간단한 설명 */}
+                      재료: { FoodIngredient[index - 1]}
+                      <br></br>
+                      <br></br>
+                      칼로리: {FoodKcal[index - 1]}
+                      영양 정보: 탄수화물 {FoodCar[index - 1] }g, 
+                      단백질 {FoodPro[index - 1] }g, 
+                      지방 {FoodFat[index - 1] }g
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">삭제</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+             :null ))}
+          </Grid>
+        </Container>
+        
         </div>
+     </ThemeProvider>   
     );
 };
 
