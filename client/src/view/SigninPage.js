62159ba7c6c68b795faacc890bd585807f555939
kunhei <84655228+kunhei@users.noreply.github.com>
2022-06-18 20:22:42 +0900

    Update SigninPage.js

--- client/src/view/SigninPage.js
+++ client/src/view/SigninPage.js
@@ -17,12 +17,9 @@ import axios from 'axios';
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+       Healthy Pleasure Site
       </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
     </Typography>
   );
 }
